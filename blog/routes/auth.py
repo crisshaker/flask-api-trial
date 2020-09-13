b724af1c9d477a202a77f95904370a2ef7a01b09
Christian Owusu <c.owusu@nsano.com>
2020-09-13 01:21:16 +0000

    added UserSchema validation logic

--- blog/routes/auth.py
+++ blog/routes/auth.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, jsonify
 from blog.database import db
 from blog.models import User
+from blog.schemas import UserSchema
 import jwt
 from blog.config import JWT_SECRET
+from marshmallow.exceptions import ValidationError
 
 blueprint = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -12,27 +14,18 @@ def register():
     # TODO: Proper Validation for username and password
 
     body = request.get_json(force=True)
+    try:
+        data = UserSchema().load(body)
+        print(data)
+    except ValidationError as e:
+        return jsonify({'errors': e.messages})
 
-    username = body.get('username')
-    password = body.get('password')
-
-    errors = {}
-
-    if not (username and len(username.strip()) > 0):
-        errors['username'] = 'Provide username'
-
-    if not (password and len(password.strip()) > 0):
-        errors['password'] = 'Provide password'
-
-    if len(errors):
-        return jsonify({'errors': errors}), 400
-
-    username = username.strip().lower()
-    username_taken = db.query(User).filter(User.username == username).first()
+    username_taken = db.query(User).filter(
+        User.username == data['username']).first()
     if username_taken:
         return jsonify({'errors': {'username': 'username already taken'}})
 
-    user = User(username=username, password=password)
+    user = User(**data)
     db.add(user)
     db.commit()
     token = jwt.encode({'user_id': user.id}, JWT_SECRET).decode('utf-8')
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,9 +1,33 @@
-from marshmallow import Schema, fields, EXCLUDE
+from marshmallow import Schema, fields, EXCLUDE, validates, ValidationError, pre_load
 
 
 class UserSchema(Schema):
-    id = fields.Integer()
-    username = fields.String()
+    id = fields.Integer(dump_only=True)
+    username = fields.String(required=True)
+    password = fields.String(required=True, load_only=True)
+    created_at = fields.DateTime(dump_only=True)
+
+    @pre_load
+    def pre_load(self, data, **kwargs):
+        if 'username' in data:
+            data['username'] = data['username'].strip().lower()
+
+        return data
+
+    @validates('username')
+    def validate_username(self, value):
+        if len(value) < 5:
+            raise ValidationError(
+                'Username must be at least 5 characters long')
+
+    @validates('password')
+    def validate_password(self, value):
+        if len(value) < 6:
+            raise ValidationError(
+                'Password must be at least 6 characters long')
+
+    class Meta:
+        unknown = EXCLUDE
 
 
 class PostSchema(Schema):
